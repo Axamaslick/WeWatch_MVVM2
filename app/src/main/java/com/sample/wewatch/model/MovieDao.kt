ca57b50462ad3c3e5a2b869ae125c2051af2ee94
Lev <kartoxauaua@gmail.com>
2025-05-13 02:40:01 +0700

    Изменение Dao

--- app/src/main/java/com/sample/wewatch/model/MovieDao.kt
+++ app/src/main/java/com/sample/wewatch/model/MovieDao.kt
@@ -1,28 +1,23 @@
 package com.sample.wewatch.model
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
-import androidx.room.OnConflictStrategy.Companion.REPLACE
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Update
-import io.reactivex.Observable
 
 @Dao
 interface MovieDao {
+  @Query("SELECT * FROM movie_table")
+  fun getAllMovies(): LiveData<List<Movie>>
 
-  @get:Query("SELECT * FROM movie_table")
-  val all: Observable<List<Movie>>
-
-  @Insert(onConflict = REPLACE)
-  fun insert(movie: Movie)
+  @Insert(onConflict = OnConflictStrategy.REPLACE)
+  suspend fun insert(movie: Movie)
 
   @Query("DELETE FROM movie_table WHERE id = :id")
-  fun delete(id: Int?)
-
-  @Query("DELETE FROM movie_table")
-  fun deleteAll()
+  suspend fun delete(id: Int?)
 
   @Update
-  fun update(movie: Movie)
-
+  suspend fun update(movie: Movie)
 }
